0c468c3b772a32e2d377da24731f45050a07c902
Chip Bell <chip.bell@spinen.com>
2014-10-06 20:15:59 -0400

    Adding detail view

---
+++ js/BookmarkletDetailView.js
@@ -0,0 +1,23 @@
+
+var Bookmarklet = require('./Bookmarklet.js');
+
+var BookmarkletDetailView = Backbone.Marionette.ItemView.extend({
+
+	modelEvents: {
+		'change' : 'render'
+	},
+
+	initialize: function() {
+		this.editor = ace.edit('editor');
+		this.editor.setTheme('ace/theme/monokai');
+		this.editor.getSession().setMode('ace/mode/javascript');
+
+		this.editor.on('change', this.refreshModelFromEditor);
+	},
+
+	refreshModelFromEditor: function() {
+		this.model = this.model || new Bookmarklet();
+
+		this.model.set('code', this.editor.getSession().getValue());
+	}
+});

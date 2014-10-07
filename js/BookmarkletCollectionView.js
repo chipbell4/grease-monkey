6fd4cf7f7404c279b71b27c5344d41444f956423
Chip Bell <chip.bell@spinen.com>
2014-10-06 21:00:19 -0400

    Fixing some load order issues

--- js/BookmarkletCollectionView.js
+++ js/BookmarkletCollectionView.js
@@ -1,3 +1,5 @@
 BookmarkletCollectionView = Backbone.Marionette.CompositeView.extend({
 	template: '#bookmarklet-collection-template',
+	childView: BookmarkletItemView,
+	childViewContainer: 'ul'
 });

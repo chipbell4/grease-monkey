cfb4e4b761d71e92787876f76762930971ddb7f8
Chip Bell <chip.bell@spinen.com>
2014-10-06 20:25:32 -0400

    Adding a gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+var CLASSES = [
+	'Bookmarklet',
+	'BookmarkletCollection',
+	'BookmarkletItemView',
+	'BookmarkletDetailView',
+	'BookmarkletCollectionView'
+];
+
+var APP_FILES = CLASSES.map(function(classname) {
+	return 'js/' + classname + '.js';
+});
+
+gulp.task('default', function() {
+	return gulp.src(APP_FILES)
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', ['default'], function() {
+	return gulp.watch(APP_FILES, ['default']);
+});

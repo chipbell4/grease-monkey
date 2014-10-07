f33585a996441ceefe8b36fe3e12557da00e285b
Chip Bell <chip.bell@spinen.com>
2014-10-07 16:48:12 -0400

    Hooking up a save button

--- js/BookmarkletDetailView.js
+++ js/BookmarkletDetailView.js
@@ -2,11 +2,25 @@ BookmarkletDetailView = Backbone.Marionette.ItemView.extend({
 
 	template: '#bookmarklet-detail-template',
 
+	events: {
+		'click .save-button' : 'onClickSave'
+	},
+
 	onRender: function() {
 		var editor_div = this.$('#editor')[0];
 
 		this.editor = ace.edit(editor_div);
 		this.editor.setTheme('ace/theme/monokai');
 		this.editor.getSession().setMode('ace/mode/javascript');
+	},
+
+	onClickSave: function() {
+		// update model from view
+		this.model.set({
+			title: this.$('[name=title]').val().trim(),
+			code: this.editor.getSession().getValue()
+		});
+
+		this.model.save();
 	}
 });

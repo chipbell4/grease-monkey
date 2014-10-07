a141871d546a8c5fd8c73fcb80373a619ad307c8
Chip Bell <chip.bell@spinen.com>
2014-10-07 16:39:17 -0400

    Fixing rendering of detail view

--- js/BookmarkletDetailView.js
+++ js/BookmarkletDetailView.js
@@ -2,17 +2,11 @@ BookmarkletDetailView = Backbone.Marionette.ItemView.extend({
 
 	template: '#bookmarklet-detail-template',
 
-	refreshModelFromEditor: function() {
-		this.model = this.model || new Bookmarklet();
-
-		this.model.set('code', this.editor.getSession().getValue());
-	},
-
 	onRender: function() {
-		this.editor = ace.edit('editor');
+		var editor_div = this.$('#editor')[0];
+
+		this.editor = ace.edit(editor_div);
 		this.editor.setTheme('ace/theme/monokai');
 		this.editor.getSession().setMode('ace/mode/javascript');
-
-		this.editor.on('change', this.refreshModelFromEditor);
 	}
 });

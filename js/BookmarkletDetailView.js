db079c09bb2b5f5de05b98890926d716e445ff24
Chip Bell <chip.bell@spinen.com>
2014-10-07 16:31:52 -0400

    Fixing some routing issues, and getting rid of event code that I wont need

--- js/BookmarkletDetailView.js
+++ js/BookmarkletDetailView.js
@@ -1,20 +1,18 @@
 BookmarkletDetailView = Backbone.Marionette.ItemView.extend({
 
-	modelEvents: {
-		'change' : 'render'
-	},
-
-	initialize: function() {
-		this.editor = ace.edit('editor');
-		this.editor.setTheme('ace/theme/monokai');
-		this.editor.getSession().setMode('ace/mode/javascript');
-
-		this.editor.on('change', this.refreshModelFromEditor);
-	},
+	template: '#bookmarklet-detail-template',
 
 	refreshModelFromEditor: function() {
 		this.model = this.model || new Bookmarklet();
 
 		this.model.set('code', this.editor.getSession().getValue());
+	},
+
+	onRender: function() {
+		this.editor = ace.edit('editor');
+		this.editor.setTheme('ace/theme/monokai');
+		this.editor.getSession().setMode('ace/mode/javascript');
+
+		this.editor.on('change', this.refreshModelFromEditor);
 	}
 });
--- js/Router.js
+++ js/Router.js
@@ -9,10 +9,20 @@ var Router = Backbone.Router.extend({
 	},
 
 	show: function(bookmarklet_id) {
+		var bookmarklet = app.Bookmarklets.get(bookmarklet_id);
+
+		// If no bookmarklet found, simply redirect
+		if(bookmarklet == null) {
+			app.router.navigate('/');
+			return;
+		}
+
 		// set the bookmarklet as the current detail view
 		app.detail.empty();
-		app.detail.show(new BookmarkletDetailView({
+		var detail_view = new BookmarkletDetailView({
 			model: app.Bookmarklets.get(bookmarklet_id)
-		}));
+		});
+		detail_view.render();
+		app.detail.show(detail_view);
 	}
 });

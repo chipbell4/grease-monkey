0f310c777db9b606c756f1a73cf5e3020a1988c2
Chip Bell <chip.bell@spinen.com>
2014-10-07 18:43:43 -0400

    Using key press to perform save

--- js/BookmarkletDetailView.js
+++ js/BookmarkletDetailView.js
@@ -3,7 +3,7 @@ BookmarkletDetailView = Backbone.Marionette.ItemView.extend({
 	template: '#bookmarklet-detail-template',
 
 	events: {
-		'click .save-button' : 'onClickSave'
+		'keyup' : 'save'
 	},
 
 	onRender: function() {
@@ -14,7 +14,7 @@ BookmarkletDetailView = Backbone.Marionette.ItemView.extend({
 		this.editor.getSession().setMode('ace/mode/javascript');
 	},
 
-	onClickSave: function() {
+	save: function() {
 		// update model from view
 		this.model.set({
 			title: this.$('[name=title]').val().trim(),

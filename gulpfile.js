d58e61484b57575e5924bd1003437a820381e0cb
Chip Bell <chip.bell@spinen.com>
2014-10-08 21:10:02 -0400

    Incorporating sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var sass = require('gulp-sass');
 
 var CLASSES = [
 	'Bookmarklet',
@@ -14,13 +15,21 @@ var CLASSES = [
 var APP_FILES = CLASSES.map(function(classname) {
 	return 'js/' + classname + '.js';
 });
+var SASS_FILES = './scss/*.scss';
 
-gulp.task('default', function() {
+gulp.task('js', function() {
 	return gulp.src(APP_FILES)
 		.pipe(concat('app.js'))
 		.pipe(gulp.dest('./'));
 });
 
+gulp.task('sass', function() {
+	return gulp.src(SASS_FILES)
+		.pipe(sass())
+		.pipe(gulp.dest('./'));
+});
+
 gulp.task('watch', ['default'], function() {
-	return gulp.watch(APP_FILES, ['default']);
+	gulp.watch(APP_FILES, ['default']);
+	gulp.watch(SASS_FILES)
 });
